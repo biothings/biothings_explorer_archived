c172ae0f2e00497f2b19e4ab7e0bdfa998c7a4de
jiwen xin <kevinxin@scripps.edu>
2019-07-10 13:53:22 -0700

    add registry parser module

--- biothings_explorer/registry.py
+++ biothings_explorer/registry.py
@@ -7,3 +7,25 @@ biothings_explorer.registry
 This module contains code that biothings_explorer use to parse the
 smartAPI registry.
 """
+import networkx as nx
+
+from .mapping_parser import MappingParser
+
+
+class Registry():
+    """Construct network"""
+    def __init__(self):
+        self.G = nx.MultiDiGraph()
+        self.BIOTHINGS = {'mygene.info': 'https://raw.githubusercontent.com/NCATS-Tangerine/translator-api-registry/openapi_2.0/mygene.info/schema.json',
+                          'myvariant.info': 'https://raw.githubusercontent.com/NCATS-Tangerine/translator-api-registry/openapi_2.0/myvariant.info/schema.json',
+                          'mychem.info': 'https://raw.githubusercontent.com/NCATS-Tangerine/translator-api-registry/openapi_2.0/mychem.info/schema.json'}
+        self.load_biothings()
+
+    def load_biothings(self):
+        self.mp = MappingParser()
+        for _api, _url in self.BIOTHINGS.items():
+            self.mp.load_mapping(_url, _api)
+            # self.G = nx.compose(self.G, self.mp.connect())
+            new_edges = self.mp.connect()
+            self.G.add_edges_from(new_edges.edges())
+        return self.G
