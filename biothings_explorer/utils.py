e7e4114d3e3f1621b959de8a355ff4bf73324e2e
jiwen xin <kevinxin@scripps.edu>
2019-07-10 13:53:40 -0700

    add utils functions

--- biothings_explorer/utils.py
+++ biothings_explorer/utils.py
@@ -7,3 +7,42 @@ biothings_explorer.utils
 This module provides utility functions that are used within bioThings_explorer
 that are also useful for external consumption.
 """
+import json
+import yaml
+
+import requests
+
+
+def load_json_or_yaml(file_path):
+    """Load either json or yaml document from file path or url or JSON doc
+
+    :arg str file_path: The path of the url doc, could be url or file path
+    """
+    # handle json doc
+    if isinstance(file_path, dict):
+        return file_path
+    # handle url
+    elif file_path.startswith("http"):
+        with requests.get(file_path) as url:
+            # check if http requests returns a success status code
+            if url.status_code != 200:
+                raise ValueError("Invalid URL!")
+            else:
+                _data = url.content
+    # handle file path
+    else:
+        try:
+            with open(file_path) as f:
+                _data = f.read()
+        except FileNotFoundError:
+            raise ValueError("Invalid File Path!")
+    try:
+        data = json.loads(_data)
+    except json.JSONDecodeError:   # for py>=3.5
+    # except ValueError:               # for py<3.5
+        try:
+            data = yaml.load(_data, Loader=yaml.SafeLoader)
+        except (yaml.scanner.ScannerError,
+                yaml.parser.ParserError):
+            raise ValueError("Not a valid JSON or YAML format.")
+    return data
