2738b7e8a49acd2d5e2fd7b9c0849c4b5d8df1f9
jiwen xin <kevinxin@scripps.edu>
2019-07-23 14:11:42 -0700

    add registry attributes

--- biothings_explorer/registry.py
+++ biothings_explorer/registry.py
@@ -19,13 +19,15 @@ class Registry():
         self.BIOTHINGS = {'mygene.info': 'https://raw.githubusercontent.com/NCATS-Tangerine/translator-api-registry/openapi_2.0/mygene.info/schema.json',
                           'myvariant.info': 'https://raw.githubusercontent.com/NCATS-Tangerine/translator-api-registry/openapi_2.0/myvariant.info/schema.json',
                           'mychem.info': 'https://raw.githubusercontent.com/NCATS-Tangerine/translator-api-registry/openapi_2.0/mychem.info/schema.json'}
+        self.registry = {}
         self.load_biothings()
 
     def load_biothings(self):
         self.mp = MappingParser()
         for _api, _url in self.BIOTHINGS.items():
+            self.registry[_api] = {}
             self.mp.load_mapping(_url, _api)
-            # self.G = nx.compose(self.G, self.mp.connect())
-            new_edges = self.mp.connect()
-            self.G.add_edges_from(new_edges.edges())
+            self.registry[_api]['mapping'] = self.mp.mapping
+            self.registry[_api]['graph'] = self.mp.connect()
+            self.G = nx.compose(self.G, self.registry[_api]['graph'])
         return self.G
