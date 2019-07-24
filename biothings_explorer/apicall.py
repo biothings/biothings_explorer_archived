172e947a02eba744158deb5f3a381257e0494292
jiwen xin <kevinxin@scripps.edu>
2019-07-24 13:38:20 -0700

    handle post API query

--- biothings_explorer/apicall.py
+++ biothings_explorer/apicall.py
@@ -12,15 +12,24 @@ import requests
 
 class BioThingsCaller():
     """call biothings APIs"""
-    def __init__(self):
+    def __init__(self, batch=False):
         self.url_pattern = {'mygene.info': "http://mygene.info/v3/query",
                             'myvariant.info': 'http://myvariant.info/v1/query',
                             "mychem.info": "http://mychem.info/v1/query"}
+        self.batch = batch
 
     def construct_query_param(self, input_fields, output_fields, value):
-        params = 'q={input}:{value}&fields={output}'
+        get_params = 'q={input}:{value}&fields={output}'
+        post_params = 'q={value}&scopes={input}&fields={output}'
+        params = post_params if self.batch else get_params
         return params.replace('{input}', input_fields).replace('{output}',output_fields).replace('{value}', value)
 
     def call_api(self, api, _input, _output, value):
         params = self.construct_query_param(_input, _output, value)
-        return requests.get(self.url_pattern[api], params=params).json()
+        if not self.batch:
+            return requests.get(self.url_pattern[api], params=params).json()
+        else:
+            headers = {'content-type': 'application/x-www-form-urlencoded'}
+            return requests.post(self.url_pattern[api],
+                                 data=params,
+                                 headers=headers).json()
