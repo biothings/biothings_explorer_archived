587e21ba8f983b61aa30c9e696f2d15ed4ed8e5e
Andrew Su <andrew.su@gmail.com>
2020-02-13 20:25:00 -0800

    rm test/test_hint.py

--- biothings_explorer/test/test_hint.py
+++
@@ -1,9 +0,0 @@
-from pytest import fixture
-
-@fixture
-def ht():
-    from biothings_explorer.hint import Hint
-    return Hint()
-
-def test_ht_gene(ht):
-    assert ht.query("CXCR4")['Gene'][0]['symbol'] == 'CXCR4'
