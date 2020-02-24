2b67cb0fd360db50c6865b296b4c0797742bcdbb
jiwen xin <kevinxin@scripps.edu>
2020-02-24 14:29:43 -0800

    return none if type not in id_rank dict

--- biothings_explorer/utils.py
+++ biothings_explorer/utils.py
@@ -35,6 +35,8 @@ def get_primary_id_from_equivalent_ids(equivalent_ids, _type):
     """
     if not equivalent_ids:
         return None
+    if _type not in id_ranks:
+        return None
     id_rank = [('bts:' + _item) for _item in id_ranks.get(_type)]
     # loop through id_rank, if the id is found in equivalent ids, return it
     for _item in id_rank:
@@ -60,54 +62,6 @@ def get_name_from_equivalent_ids(equivalent_ids):
         return equivalent_ids.get('bts:name')[0]
     else:
         return None
-
-def restructure_gwascatalog(json_doc):
-    """restructure gwascatalog"""
-    if json_doc:
-        associations = json_doc.get('gwascatalog').get("associations")
-        if type(associations) == dict:
-            associations = [associations]
-        for _assoc in associaitons:
-            efo = _assoc.get("efo")
-            if efo:
-                efo_id = efo.get("id")
-                if efo_id:
-                    efo_id = efo_id.split(':')[-1]
-        return json_doc
-
-def restructure_biolink_response(json_doc):
-    """
-    ANATOMY: UBERON, CL, FBbt
-    DISEASE: MONDO
-    GENE: HGNC, NCBIGene, MGI， ZFIN，FlyBase
-    PHENOTYPE: EFO, HP, MONDO
-    """
-    if json_doc and 'associations' in json_doc:
-        for _doc in json_doc['associations']:
-            # remove prefix
-            if 'object' in _doc and 'id' in _doc['object']:
-                object_id = _doc['object']['id']
-                try:
-                    prefix, value = object_id.split(':')
-                    # these IDs have prefix by nature
-                    if prefix in ['HGNC', 'NCBIGene']:
-                        _doc['object'][prefix] = value
-                    else:
-                        _doc['object'][prefix] = object_id
-                except:
-                    pass
-            # remove empty value
-            if not _doc['publications']:
-                _doc.pop('publications')
-            else:
-                for _item in _doc['publications']:
-                    _item['id'] = _item['id'].split(':')[-1]
-            if not _doc['provided_by']:
-                _doc.pop('provided_by')
-            else:
-                for i, _item in enumerate(_doc['provided_by']):
-                    _doc['provided_by'][i] = _item.split(".")[-2].split("/")[-1]
-    return json_doc
 
 
 def visualize(edges, size=None):
