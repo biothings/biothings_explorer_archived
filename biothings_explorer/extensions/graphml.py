a3f3e5718aa8b690c6b3038632024c99ee3cdb4a
jiwen xin <kevinxin@scripps.edu>
2020-03-10 21:05:07 -0700

    codacy fix

--- biothings_explorer/extensions/graphml.py
+++ biothings_explorer/extensions/graphml.py
@@ -9,19 +9,20 @@
 import copy
 import networkx as nx
 
+
 class GraphmlConverter():
 
     def load_bte_output(self, G):
-        """Load bte output in the format of networkx graph into class.
-        
-        parameters
-        ----------
-        G: the networkx representation of bte output
+        """
+        Load bte output in the format of networkx graph into class.
+
+        :param: G : the networkx representation of bte output
         """
         self.G = copy.deepcopy(G)
 
     def restructure_node_info(self):
-        """Restructure node info.
+        """
+        Restructure node info.
 
         note: graphml doesn't support dict as output
         """
@@ -33,9 +34,10 @@ class GraphmlConverter():
             for m, n in v.items():
                 if type(n) == list:
                     v[m] = ','.join([str(_item) for _item in n])
-    
+
     def restructure_edge_info(self):
-        """Restructure edge info.
+        """
+        Restructure edge info.
 
         note: graphml doesn't support dict as output
         """
@@ -44,18 +46,16 @@ class GraphmlConverter():
                 o.update(o['info'])
                 del o['info']
             for m, n in o.items():
-                print(m,n)
                 if type(n) == list:
                     o[m] = ','.join([str(_item) for _item in n])
                 if not n:
                     o[m] = 'null'
-   
+
     def generate_graphml_output(self, path):
-        """Return the graphml representation of bte output.
-        
-        parameters
-        ----------
-        path (str) : the file path to store graphml file
+        """
+        Return the graphml representation of bte output.
+
+        :param: path (str) : the file path to store graphml file
         """
         self.restructure_edge_info()
         self.restructure_node_info()
