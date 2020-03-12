a23f4cf23288dfaed047accb92b0fef04370110f
jiwen xin <kevinxin@scripps.edu>
2020-03-12 10:47:11 -0700

    update stanford biosample preprocess module

--- biothings_explorer/api_preprocess/stanford.py
+++ biothings_explorer/api_preprocess/stanford.py
@@ -7,21 +7,24 @@ def restructure_stanford_response(json_doc):
     # convert from list to dict
     if isinstance(json_doc, list):
         json_doc = {'data': json_doc}
-    for _doc in json_doc['data']:
-        # store restructured attributes info
-        new_attr = {}
-        for _attr in _doc['attributes']:
-            if 'attributeValueTermUri' not in _attr:
-                new_attr[_attr['attributeName'].replace(' ', '_')] = _attr['attributeValue']
-            else:
-                new_attr[_attr['attributeName'].replace(' ', '_')] = {'name': _attr['attributeValue']}
-                if 'PATO_' in _attr['attributeValueTermUri']:
-                    new_attr[_attr['attributeName'].replace(' ', '_')]['pato'] = 'PATO:' + _attr['attributeValueTermUri'].split('_')[-1]
-                elif 'MONDO_' in _attr['attributeValueTermUri']:
-                    new_attr[_attr['attributeName'].replace(' ', '_')]['mondo'] = 'MONDO:' + _attr['attributeValueTermUri'].split('_')[-1]
-                elif 'BTO_' in _attr['attributeValueTermUri']:
-                    new_attr[_attr['attributeName'].replace(' ', '_')]['bto'] = 'BTO:' + _attr['attributeValueTermUri'].split('_')[-1]
-                elif 'CLO_' in _attr['attributeValueTermUri']:
-                    new_attr[_attr['attributeName'].replace(' ', '_')]['clo'] = 'CLO:' + _attr['attributeValueTermUri'].split('_')[-1]
-        _doc['attributes'] = new_attr
-    return json_doc
+    if 'data' in json_doc:
+        for _doc in json_doc['data']:
+            # store restructured attributes info
+            new_attr = {}
+            for _attr in _doc['attributes']:
+                if 'attributeValueTermUri' not in _attr:
+                    new_attr[_attr['attributeName'].replace(' ', '_')] = _attr['attributeValue']
+                else:
+                    new_attr[_attr['attributeName'].replace(' ', '_')] = {'name': _attr['attributeValue']}
+                    if 'PATO_' in _attr['attributeValueTermUri']:
+                        new_attr[_attr['attributeName'].replace(' ', '_')]['pato'] = 'PATO:' + _attr['attributeValueTermUri'].split('_')[-1]
+                    elif 'MONDO_' in _attr['attributeValueTermUri']:
+                        new_attr[_attr['attributeName'].replace(' ', '_')]['mondo'] = 'MONDO:' + _attr['attributeValueTermUri'].split('_')[-1]
+                    elif 'BTO_' in _attr['attributeValueTermUri']:
+                        new_attr[_attr['attributeName'].replace(' ', '_')]['bto'] = 'BTO:' + _attr['attributeValueTermUri'].split('_')[-1]
+                    elif 'CLO_' in _attr['attributeValueTermUri']:
+                        new_attr[_attr['attributeName'].replace(' ', '_')]['clo'] = 'CLO:' + _attr['attributeValueTermUri'].split('_')[-1]
+            _doc['attributes'] = new_attr
+        return json_doc
+    else:
+        return
