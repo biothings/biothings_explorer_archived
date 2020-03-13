59d904825b160e6d3ba61e25d9323c885b8ec031
jiwen xin <kevinxin@scripps.edu>
2020-03-13 14:41:12 -0700

    codacy fix

---
+++ biothings_explorer/extensions/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Extend BioThings Explorer to other format, e.g. reasonerStdAPI, graphml.
+
+.. moduleauthor:: Jiwen Xin <kevinxin@scripps.edu>
+
+
+"""
---
+++ tests/test_api_preprocess/test_biolink_preproces.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Test the api preprocess module for biolink.
+
+.. moduleauthor:: Jiwen Xin <kevinxin@scripps.edu>
+
+
+"""
