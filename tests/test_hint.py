a01df9b3eba2864b756c73c29ec91cd9d64786e0
jiwen xin <kevinxin@scripps.edu>
2020-03-13 15:42:05 -0700

    simplify python unittest

--- tests/test_hint.py
+++ tests/test_hint.py
@@ -6,9 +6,7 @@ class TestHint(unittest.TestCase):
     def setUp(self):
         self.ht = Hint()
 
-    def test_gene_entrez_id_as_input(self):
-        """Test the output of Hint query when providing gene entrez id as input."""
-        res = self.ht.query('1017')
+    def mygene(self, res):
         self.assertIsNotNone(res)
         self.assertIsNotNone(res.get('Gene'))
         self.assertIsNotNone(res.get('Gene')[0])
@@ -18,6 +16,22 @@ class TestHint(unittest.TestCase):
         self.assertEqual(bioentity['primary']['identifier'], 'entrez')
         self.assertEqual(bioentity['primary']['value'], '1017')
         self.assertEqual(bioentity['symbol'], 'CDK2')
+
+    def test_gene_entrez_id_as_input(self):
+        """Test the output of Hint query when providing gene entrez id as input."""
+        res = self.ht.query('1017')
+        """
+        self.assertIsNotNone(res)
+        self.assertIsNotNone(res.get('Gene'))
+        self.assertIsNotNone(res.get('Gene')[0])
+        bioentity = res.get('Gene')[0]
+        self.assertEqual(bioentity['entrez'], '1017')
+        self.assertEqual(bioentity['type'], 'Gene')
+        self.assertEqual(bioentity['primary']['identifier'], 'entrez')
+        self.assertEqual(bioentity['primary']['value'], '1017')
+        self.assertEqual(bioentity['symbol'], 'CDK2')
+        """
+        self.mygene(res)
 
     def test_gene_symbol_as_input(self):
         """Test the output of Hint query when providing gene symbol as input."""
