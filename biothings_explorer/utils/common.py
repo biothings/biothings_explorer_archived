98bec91c772db418db33e63fb370ed7b6cba5924
jiwen xin <kevinxin@scripps.edu>
2020-03-19 09:58:14 -0700

    flake8 fix

--- biothings_explorer/utils/common.py
+++ biothings_explorer/utils/common.py
@@ -1,9 +1,12 @@
 """This file covers some common utils functions for BioThings Explorer"""
+
 from os.path import commonprefix
+from ..config import id_ranks
+
 
 def add_s(num: int) -> str:
     """Add 's' if num is more than one.
-    
+
     :param: num: An integer representing count
     """
     if not isinstance(num, int):
@@ -13,27 +16,30 @@ def add_s(num: int) -> str:
     else:
         return 's'
 
+
 def dict2listoftuples(py_dict: dict) -> list:
     """Convert a single python dictionary into a list of tuples.
-    
+
     :param: py_dict: a single python dictionary
     """
     return [(k, v) for k, v in py_dict.items()]
 
 
 def listoftuples2dict(tuple_list: list) -> dict:
-    """Convert a list of tuples back to a single dictionary
-    
+    """Convert a list of tuples back to a single dictionary.
+
     :param: tuple_list: a list of tuples
     """
     try:
         return dict(tuple_list)
     except ValueError:
-        raise ValueError("The input must a list of tuples with each tuple of length 2")
+        raise ValueError("The input must a list of tuples \
+                         with each tuple of length 2")
+
 
 def unlist(d: dict) -> dict:
-    """Find all appearance of single element list in a dictionary and unlist it
-    
+    """Find all appearance of single element list in a dictionary and unlist it.
+
     :param: d: a python dictionary to be unlisted
     """
     if type(d) == list:
@@ -53,18 +59,69 @@ def unlist(d: dict) -> dict:
         return d
 
 
-def find_longest_common_path(paths):
+def find_longest_common_path(paths) -> str:
     """Find longest common path among a list of paths.
-    
+
     :param: paths: a list of paths, with '.' as the separator
     """
     return commonprefix(paths).rsplit('.', 1)[0]
 
 
-def get_dict_values(py_dict: dict, excluded_keys: list = ["@type", "@input", "$source"]):
-    """Retrieve the values of a python dictionary
-    
+def get_dict_values(py_dict: dict,
+                    excluded_keys: list = ["@type", "@input",
+                                           "$source"]) -> list:
+    """Retrieve the values of a python dictionary.
+
     :param: py_dict: a python dictionary
-    :param: excluded_keys: a list of keys to be excluded, meaning the values of these keys should be excluded
+    :param: excluded_keys: a list of keys to be excluded, \
+        meaning the values of these keys should be excluded
     """
     return [v for k, v in py_dict.items() if k not in excluded_keys]
+
+
+def get_primary_id_from_equivalent_ids(equivalent_ids, _type):
+    """find primary id from equivalent id dict
+
+    params
+    ------
+    equivalent_ids: a dictionary containing all equivalent ids of a bio-entity
+    _type: the type of the bio-entity
+    """
+    if not equivalent_ids:
+        return None
+    if _type in id_ranks:
+        id_rank = [('bts:' + _item) for _item in id_ranks.get(_type)]
+        # loop through id_rank, if the id is found in equivalent ids, return it
+        for _item in id_rank:
+            if equivalent_ids.get(_item):
+                return (_item[4:] + ':' + str(equivalent_ids[_item][0]))
+    # if no id found, return a random one from equivalent ids
+    for k, v in equivalent_ids.items():
+        if v:
+            return (k[4:] + ':' + str(v[0]))
+
+
+def get_name_from_equivalent_ids(equivalent_ids, input_label):
+    """find name from equivalent id dict
+
+    params
+    ------
+    equivalent_ids: a dictionary containing all equivalent ids of a bio-entity
+    input_label: desginated input_label
+    """
+    if input_label:
+        return input_label
+    if not equivalent_ids:
+        return "unknown"
+    if equivalent_ids.get('bts:symbol'):
+        return equivalent_ids.get('bts:symbol')[0]
+    elif equivalent_ids.get('bts:name'):
+        return equivalent_ids.get('bts:name')[0]
+    else:
+        for v in equivalent_ids.values():
+            if v:
+                if type(v) == list:
+                    return v[0]
+                else:
+                    return v
+        return "unknown"
