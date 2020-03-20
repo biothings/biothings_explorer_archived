4142f7ec86ad5efdc0229156d1296c9e886b9023
jiwen xin <kevinxin@scripps.edu>
2020-03-20 10:53:35 -0700

    pylint fix

--- biothings_explorer/smartapi_parser.py
+++ biothings_explorer/smartapi_parser.py
@@ -6,37 +6,35 @@ class SmartAPIParser():
         self.info = {"endpoints": []}
 
     def fetch_api_name(self):
-        """fetch the name of the API
+        """Fetch the name of the API.
         """
         return self.spec['info']['title']
 
     def fetch_server_url(self):
-        """fetch the server url of the API
+        """fetch the server url of the API.
         """
         return self.spec['servers'][0]['url']
 
     def fetch_api_tags(self):
-        """fetch the api tags info of the API
+        """Fetch the api tags info of the API.
         """
         if "tags" in self.spec:
             return [_item['name'] for _item in self.spec['tags']]
-        else:
-            return []
+        return []
 
     def fetch_semantic_mapping_path(self, endpoint_spec):
-        """fetch the path of semantic mapping json doc
+        """Fetch the path of semantic mapping json doc.
         """
         return endpoint_spec["x-bte-semanticmapping"]["$ref"]
     
     def fetch_semantic_mapping(self, ref):
-        """fetch the semantic mapping json file based on the path in $ref
+        """Fetch the semantic mapping json file based on the path in $ref.
         """
         path = ref.split('/')
-        assert path[1] == "components"
-        assert path[2] == "x-bte-semanticmapping"
         return self.spec["components"]["x-bte-semanticmapping"][path[-1]]
     
-    def fetch_param(self, endpoint_spec):
+    @staticmethod
+    def fetch_param(endpoint_spec):
         """fetch the parameter name of the endpoint
         
         params
@@ -45,8 +43,7 @@ class SmartAPIParser():
         """
         if 'parameters' in endpoint_spec:
             return endpoint_spec['parameters'][0].get('name')
-        else:
-            return None
+        return None
 
     def fetch_endpoint_info(self):
         endpoint_summary = []
@@ -62,12 +59,3 @@ class SmartAPIParser():
                     method_summary['mapping'] = self.fetch_semantic_mapping(mapping_path)
                     endpoint_summary.append(method_summary)
         return endpoint_summary
-
-    
-
-    
-
-
-
-
-    
