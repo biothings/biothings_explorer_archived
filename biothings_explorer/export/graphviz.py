d3eec17b2d77d178c1fcf52a67de2381efdb5354
jiwen xin <kevinxin@scripps.edu>
2020-03-20 07:03:58 -0700

    fix codacy syntax issue

--- biothings_explorer/export/graphviz.py
+++ biothings_explorer/export/graphviz.py
@@ -10,8 +10,8 @@ from graphviz import Digraph
 
 
 def visualize(edges, size=None):
-    """Export networkx graph into graphviz format for jupyter notebook visualization
-    
+    """Export networkx graph into graphviz format for jupyter notebook visualization.
+
     :param: edges: the edge representation of the graph
     :param: size: the size of the output graph, e.g. (6, 8)
     """
