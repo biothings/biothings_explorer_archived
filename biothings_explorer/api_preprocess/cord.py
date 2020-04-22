06850be1d31b20c9cd83f2a46558710c8ccf3a0e
jiwen xin <kevinxin@scripps.edu>
2020-04-21 20:40:28 -0700

    update cord preprocessor

--- biothings_explorer/api_preprocess/cord.py
+++ biothings_explorer/api_preprocess/cord.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 def restructure_cord_response(json_doc, output_types):
     """Restructure the JSON output from cord API.
 
@@ -12,15 +14,20 @@ def restructure_cord_response(json_doc, output_types):
         # handle case where the queried item is not found
         if _res.get('notfound'):
             continue
-        else:
-            tmp_res = {'query': _res['query']}
-            for k, v in _res.items():
-                tmp_v = []
-                if isinstance(v, list):
-                    for item in v:
-                        if item["@type"] in  output_types:
-                            tmp_v.append(item)
-                if tmp_v != []:
-                    tmp_res[k] = tmp_v
-            new_res.append(tmp_res)
+        tmp_res = {'query': _res['query']}
+        for k, v in _res.items():
+            if k == "associated_with":
+                k = "related_to"
+            tmp_v = []
+            if isinstance(v, list):
+                for item in v:
+                    if item["@type"] in output_types or (item["@type"] == 'DiseaseOrPhenotypicFeature' and output_types == ['Disease']):
+                        item_copy = deepcopy(item)
+                        for m in item.keys():
+                            if m in ['pr', 'go', 'mop', 'hgnc', 'uberon', 'so', 'cl', 'doid', 'chebi']:
+                                item_copy[m.upper()] = item_copy.pop(m)
+                        tmp_v.append(item_copy)
+            if tmp_v != []:
+                tmp_res[k] = tmp_v
+        new_res.append(tmp_res)
     return new_res
