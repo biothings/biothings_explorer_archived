10b2aa003941cdfa90718e399df269f3c38369f6
jiwen xin <kevinxin@scripps.edu>
2020-04-25 15:54:27 -0700

    add get primary id and type

--- biothings_explorer/hint.py
+++ biothings_explorer/hint.py
@@ -16,16 +16,14 @@ class Hint():
         """Load BTE API caller."""
         self.caller = BioThingsCaller()
 
-    @staticmethod
-    def get_primary_id(semantic_type, json_doc):
+    def get_primary_id(self, semantic_type, json_doc):
         """Get the primary id of a biological entity
         
-        :param: client: the name of the API
         :param: json_doc: the API output
-        :param: _type: the main entity type of the output, e.g. Gene, SequenceVariant
+        :param: semantic_type: the main entity type of the output, e.g. Gene, SequenceVariant
         """
         # parse the id rank info from metadata
-        ranks = ID_RESOLVING_APIS[semantic_type]['id_ranks']
+        ranks = self.ID_RESOLVING_APIS[semantic_type]['id_ranks']
         res = {}
         # loop through the id rank list, e.g. ['chembl', 'drugbank', ...]
         # the id rank list is ranked based on priorirty
@@ -38,18 +36,39 @@ class Hint():
                 break
         return res
 
+    def get_display_message(self, semantic_type, json_doc):
+        """Get the primary id of a biological entity
+        
+        :param: semantic_type: the main entity type of the output, e.g. Gene, SequenceVariant
+        :param: json_doc: the output
+        """
+        # parse the id rank info from metadata
+        res = ''
+        for _id in self.ID_RESOLVING_APIS[semantic_type]["id_ranks"]:
+            if _id in json_doc:
+                res += (_id + '(' + json_doc[_id]  + ') ')
+        return res.strip(' ')
+
     @staticmethod
     def get_query_fields(mapping_file):
+        """Parse the mapping file to retrieve fields for query"""
         fields = []
         for v in mapping_file.values():
             fields += v
         return ','.join(fields)
 
-    def query(self, _input):
+    def query(self, _input, semantic_type=None):
         """Main function to resolve identifiers.
         
-        :param: inputs: A list of tuples, [(ids, prefix, semantic_type), ...]
+        :param: _input: any string input
         """
+        if semantic_type:
+            if semantic_type not in ID_RESOLVING_APIS:
+                raise ValueError("{} is not a valid semantic type to resolve. Available semantic types are {}".format(semantic_type, ','.join(ID_RESOLVING_APIS.keys())))
+            else:
+                self.ID_RESOLVING_APIS = {semantic_type: ID_RESOLVING_APIS[semantic_type]}
+        else:
+            self.ID_RESOLVING_APIS = ID_RESOLVING_APIS
         self.construct_api_calls(_input)
         # make API calls asynchronously and gather all outputs
         self.responses, _ = self.caller.call_apis(self.api_call_inputs)
@@ -57,13 +76,14 @@ class Hint():
 
     def parse_api_responses(self):
         """Parse the API responses from APICall module in BTE."""
+        # bp,  pathway,  cc, mf shares the same API for id resolving
         TYPE_MAPPING = {
             "bp": "BiologicalProcess",
             "pathway": "Pathway",
             "cc": "CellularComponent",
             "mf": "MolecularActivity"
         }
-        parsed_response = {k: [] for k in ID_RESOLVING_APIS}
+        parsed_response = {k: [] for k in self.ID_RESOLVING_APIS}
         for _res in self.responses:
             # if API response is empty, continue
             if not _res:
@@ -88,7 +108,10 @@ class Hint():
                                 tmp[k] = val[0]
                             break
                 tmp['primary'] = self.get_primary_id(semantic_type, tmp)
+                tmp['display'] = self.get_display_message(semantic_type, tmp)
+                tmp['type'] = semantic_type
                 parsed_response[semantic_type].append(tmp)
+
         return parsed_response                
                     
     def construct_api_calls(self, _id):
@@ -96,7 +119,7 @@ class Hint():
         """
         self.api_call_inputs = []
         unique_base_urls = set()
-        for semantic_type, api_data in ID_RESOLVING_APIS.items():
+        for semantic_type, api_data in self.ID_RESOLVING_APIS.items():
             server_url = api_data.get("url")
             if server_url in unique_base_urls:
                 continue
