ae8e7506b29dd3c15a1650b47c7357add345e51c
jiwen xin <kevinxin@scripps.edu>
2020-04-25 16:56:55 -0700

    add new test for id resolver

--- tests/test_id_resolver.py
+++ tests/test_id_resolver.py
@@ -15,11 +15,18 @@ class TestIDResolver(unittest.TestCase):
         self.assertEqual(res['SYMBOL:CXCR4']['NCBIGene'], ['7852'])
         self.assertEqual(res['SYMBOL:CXCR4']['SYMBOL'], ['CXCR4'])
 
-    def test_wrong_gene_SYMBOLss(self):
+    def test_wrong_gene_SYMBOLs(self):
         inputs = [(['CXCR4', 'CXCR3', 'CXCR2', '123'], 'SYMBOL', 'Gene')]
         res = self.idr.resolve_ids(inputs)
         self.assertIn('SYMBOL:123', res)
         self.assertDictEqual(res['SYMBOL:123'], {'SYMBOL': ['123']})
+
+    def test_pathway_as_input(self):
+        inputs = [(["R-HSA-109582"], 'Reactome', 'Pathway')]
+        res = self.idr.resolve_ids(inputs)
+        self.assertIn('Reactome:R-HSA-109582', res)
+        self.assertEqual(res['Reactome:R-HSA-109582']['name'], ['HEMOSTASIS'])
+        self.assertEqual(res['Reactome:R-HSA-109582']['Reactome'], ['R-HSA-109582'])
 
 
 if __name__ == '__main__':
