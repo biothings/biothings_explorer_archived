6dcdc73655ea3602116a3f8555eaf514e98aa739
jiwen xin <kevinxin@scripps.edu>
2020-04-29 16:02:48 -0700

    add test for user query dispatcher

---
+++ tests/test_user_query_dispatcher.py
@@ -0,0 +1,16 @@
+import unittest
+from biothings_explorer.user_query_dispatcher import FindConnection
+from biothings_explorer.hint import Hint
+
+ht = Hint()
+
+class TestSingleHopQuery(unittest.TestCase):
+
+    def test_result_section(self):
+        """Find connection between TMPRSS2 and pentamidine through all intermediate nodes """
+        tmprss2 = ht.query("TMPRSS2")['Gene'][0]
+        pentamidine = ht.query("pentamidine")['ChemicalSubstance'][0]
+        fc = FindConnection(input_obj=tmprss2, output_obj=pentamidine, intermediate_nodes=['BiologicalEntity'], registry=None)
+        fc.connect(verbose=True)
+        self.assertIn('PLXNA2', fc.fc.G)
+
