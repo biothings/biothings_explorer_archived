9d4707b793463e8d5e871ddaad149224e0b3f3ff
jiwen xin <kevinxin@scripps.edu>
2020-05-13 09:15:32 -0700

    add test for the fixed issue

--- tests/test_user_query_dispatcher.py
+++ tests/test_user_query_dispatcher.py
@@ -4,12 +4,34 @@ from biothings_explorer.hint import Hint
 
 ht = Hint()
 
-class TestUserQueryDispatcher(unittest.TestCase):
 
+class TestUserQueryDispatcher(unittest.TestCase):
     def test_result_section(self):
         """Find connection between TMPRSS2 and pentamidine through all intermediate nodes """
-        tmprss2 = ht.query("TMPRSS2")['Gene'][0]
-        pentamidine = ht.query("pentamidine")['ChemicalSubstance'][0]
-        fc = FindConnection(input_obj=tmprss2, output_obj=pentamidine, intermediate_nodes=['BiologicalEntity'], registry=None)
+        tmprss2 = ht.query("TMPRSS2")["Gene"][0]
+        pentamidine = ht.query("pentamidine")["ChemicalSubstance"][0]
+        fc = FindConnection(
+            input_obj=tmprss2,
+            output_obj=pentamidine,
+            intermediate_nodes=["BiologicalEntity"],
+            registry=None,
+        )
         fc.connect(verbose=True)
-        self.assertIn('PLXNA2', fc.fc.G)
+        self.assertIn("PLXNA2", fc.fc.G)
+
+    def test_query_with_broken_intermediate_nodes(self):
+        """For a query with long intermediate nodes, it might happen that one intermediate query returns 0 hits.
+        In this case, we should stop the code execution"""
+        mof = ht.query("Multiple Organ Failure")["Disease"][0]
+        fc = FindConnection(
+            mof,
+            output_obj="Gene",
+            intermediate_nodes=[
+                "BiologicalProcess",
+                "Cell",
+                "AnatomicalEntity",
+                "CellularComponent",
+            ],
+        )
+        fc.connect()
+        self.assertGreater(len(fc.fc.G), 2)
