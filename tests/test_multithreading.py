7f567ae76d38d3283ccd6f4e1a609abdfa4b9e1a
jiwen xin <kevinxin@scripps.edu>
2020-05-28 20:14:40 -0700

    add test for multithreading

---
+++ tests/test_multithreading.py
@@ -0,0 +1,35 @@
+import threading
+import asyncio
+import unittest
+from biothings_explorer.user_query_dispatcher import SingleEdgeQueryDispatcher
+
+
+def thread_function(index, result):
+    loop = asyncio.new_event_loop()
+    seqd = SingleEdgeQueryDispatcher(
+        output_cls="Disease",
+        input_id="MESH",
+        output_id="MONDO",
+        input_cls="ChemicalSubstance",
+        pred="related_to",
+        values="D014801",
+        loop=loop,
+    )
+    seqd.query()
+    result[index] = seqd.G
+
+
+class TestMultiThreading(unittest.TestCase):
+    def test_multithreading(self):
+        threads = list()
+        results = [None] * 3
+        for index in range(3):
+            x = threading.Thread(target=thread_function, args=(index, results))
+            threads.append(x)
+            x.start()
+
+        for index, thread in enumerate(threads):
+            thread.join()
+        self.assertIn("MONDO:0013231", results[0])
+        self.assertIn("MONDO:0013231", results[1])
+        self.assertIn("MONDO:0013231", results[2])
