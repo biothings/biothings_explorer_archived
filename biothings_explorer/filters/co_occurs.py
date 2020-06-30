b61e30ff00c3eb77ef3b171bfb544ca84e5d2c80
luluricketts <55892764+luluricketts@users.noreply.github.com>
2020-06-30 15:47:35 -0700

    fixed API call

--- biothings_explorer/filters/co_occurs.py
+++ biothings_explorer/filters/co_occurs.py
@@ -18,32 +18,15 @@ Returns:
 
 import requests
 
-def filter_co_occur(G, count=50):
-
-    # helper methods
-    def get_ids(node):
-        ids = []
-        try:
-            ids.append(G.nodes[node]['equivalent_ids']['MESH'])
-            ids.append(G.nodes[node]['equivalent_ids']['UMLS'])
-        except:
-            pass
-        ids = [i for sub in ids for i in sub] # flatten and get rid of set()
-        return 0 if len(ids) == 0 else ids
-
-    def make_combo(id1, id2):
-        combos = ['-'.join([i,j]) for i in id1 for j in id2]
-        combos += ['-'.join([j,i]) for i in id1 for j in id2]
-        return combos
-
-    # begin code
+def filter2(G, count=50):
     unique_edges = []
-    for edge in G.edges:
+    for edge in seqd.G.edges:
         if [edge[0], edge[1]] in unique_edges:
             continue
         else:
             unique_edges.append([edge[0], edge[1]])
 
+    num_combs, combos = [], []
     for edge in unique_edges:
         id1 = get_ids(edge[0])
         id2 = get_ids(edge[1])
@@ -52,13 +35,28 @@ def filter_co_occur(G, count=50):
             edge.insert(0, 100)
         else:
             combo = make_combo(id1, id2)
-            x = requests.post('https://biothings.ncats.io/mrcoc/query', json={'scopes':'combo', 'q': combo}).json()
-            for query in x:
-                if not 'notfound' in query:
-                    edge.insert(0, query['ngd_overall'])
-                    break
-            if not isinstance(edge[0], float):
-                edge.insert(0, 200)
+            combos.append(combo)
+            num_combs.append(len(combo))
+
+    combos = [i for j in combos for i in j]
+    chunks = [combos[x:x+1000] for x in range(0,len(combos),1000)]
+    x = []
+    for chunk in chunks:
+        x += requests.post('https://biothings.ncats.io/mrcoc/query', json={'scopes':'combo', 'q': chunk}).json()
+
+    end, i = 0, 0
+    for edge in unique_edges:
+        if isinstance(edge[0],int):
+            continue
+        start = end
+        end += num_combs[i]
+        for query in x[start:end]:
+            if not 'notfound' in query:
+                edge.insert(0, query['ngd_overall'])
+                break
+        if not isinstance(edge[0], float):
+            edge.insert(0, 200)
+        i+=1
 
     results = sorted(unique_edges)[:count]
     filtered = list(set([i[1] for i in results] + [i[2] for i in results]))
