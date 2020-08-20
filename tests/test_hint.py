ac47e78d39e22c0eec8e5197b11818fa2cd69f50
jiwen xin <kevinxin@scripps.edu>
2020-08-20 11:05:38 -0700

    add test for hint module

--- tests/test_hint.py
+++ tests/test_hint.py
@@ -83,6 +83,71 @@ class TestHint(unittest.TestCase):
         self.assertIsNotNone(res)
         self.assertEqual(bioentity["name"], "Carboxymycobactin S")
 
+    def test_chemical_with_cas_id(self):
+        """Test the output of Hint query when providing chemical CAS ID as input."""
+        res = self.ht.query("1744-22-5")
+        bioentity = res.get("ChemicalSubstance")[0]
+        self.assertIsNotNone(res)
+        self.assertEqual(bioentity["CHEMBL.COMPOUND"], "CHEMBL744")
+
+    def test_chemical_with_iupac_id(self):
+        """Test the output of Hint query when providing chemical IUPAC ID as input."""
+        res = self.ht.query("[6-(trifluoromethoxy)-1,3-benzothiazol-2-yl]amine")
+        bioentity = res.get("ChemicalSubstance")[0]
+        self.assertIsNotNone(res)
+        self.assertEqual(
+            bioentity["IUPAC"], "[6-(trifluoromethoxy)-1,3-benzothiazol-2-yl]amine"
+        )
+
+    def test_chemical_with_formula(self):
+        """Test the output of Hint query when providing chemical formula as input."""
+        res = self.ht.query("H2O")
+        bioentity = res.get("ChemicalSubstance")[0]
+        self.assertIsNotNone(res)
+        self.assertEqual(bioentity["formula"], "H2O")
+
+    def test_chemical_with_abbreviations(self):
+        """Test the output of Hint query when providing chemical abbreviations as input."""
+        res = self.ht.query("AZT")
+        names = [item.get("name") for item in res.get("ChemicalSubstance")]
+        self.assertIsNotNone(res)
+        self.assertIn("zidovudine", names)
+
+    def test_chemical_with_international_brand_names(self):
+        """Test the output of Hint query when providing drug international brand name as input."""
+        res = self.ht.query("Dulane")
+        names = [item.get("name") for item in res.get("ChemicalSubstance")]
+        self.assertIsNotNone(res)
+        self.assertIn("DULOXETINE", names)
+
+    def test_mf_with_lowercase_go_term(self):
+        """Test the output of Hint query when providing lower case go term as input."""
+        res = self.ht.query("go:0001790")
+        bioentity = res.get("MolecularActivity")[0]
+        self.assertIsNotNone(res)
+        self.assertEqual(bioentity["GO"], "GO:0001790")
+
+    def test_bp_with_lowercase_go_term(self):
+        """Test the output of Hint query when providing lower case go term as input."""
+        res = self.ht.query("go:0002354")
+        bioentity = res.get("BiologicalProcess")[0]
+        self.assertIsNotNone(res)
+        self.assertEqual(bioentity["GO"], "GO:0002354")
+
+    def test_cc_with_lowercase_go_term(self):
+        """Test the output of Hint query when providing lower case go term as input."""
+        res = self.ht.query("go:1903503")
+        bioentity = res.get("CellularComponent")[0]
+        self.assertIsNotNone(res)
+        self.assertEqual(bioentity["GO"], "GO:1903503")
+
+    def test_anatomy_with_uberon_id(self):
+        """Test the output of Hint query when providing UBERON ID as input."""
+        res = self.ht.query("UBERON:0035924")
+        bioentity = res.get("AnatomicalEntity")[0]
+        self.assertIsNotNone(res)
+        self.assertEqual(bioentity["UBERON"], "UBERON:0035924")
+
     def test_resolving_by_synonyms(self):
         """Test the output of Hint query when providing disease synonyms"""
         res = self.ht.query("GIST")
