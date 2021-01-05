17ce7b57f7ad9094da572802bbb429c6af0bf0c8
jiwen xin <kevinxin@scripps.edu>
2021-01-05 15:37:05 -0800

    add utils

--- biothings_explorer/utils/common.py
+++ biothings_explorer/utils/common.py
@@ -2,6 +2,7 @@
 
 from os.path import commonprefix
 from ..config import id_ranks, INTERNAL_KEYS
+from ..config_new import ALWAYS_PREFIXED
 
 
 def add_s(num: int) -> str:
@@ -10,10 +11,10 @@ def add_s(num: int) -> str:
     :param: num: An integer representing count
     """
     if not isinstance(num, int):
-        return ''
+        return ""
     if num <= 1:
-        return ''
-    return 's'
+        return ""
+    return "s"
 
 
 def dict2listoftuples(py_dict: dict) -> list:
@@ -32,8 +33,10 @@ def listoftuples2dict(tuple_list: list) -> dict:
     try:
         return dict(tuple_list)
     except ValueError:
-        raise ValueError("The input must a list of tuples \
-                         with each tuple of length 2")
+        raise ValueError(
+            "The input must a list of tuples \
+                         with each tuple of length 2"
+        )
 
 
 def unlist(d: dict) -> dict:
@@ -61,11 +64,10 @@ def find_longest_common_path(paths) -> str:
 
     :param: paths: a list of paths, with '.' as the separator
     """
-    return commonprefix(paths).rsplit('.', 1)[0]
+    return commonprefix(paths).rsplit(".", 1)[0]
 
 
-def get_dict_values(py_dict: dict,
-                    excluded_keys: list = INTERNAL_KEYS) -> list:
+def get_dict_values(py_dict: dict, excluded_keys: list = INTERNAL_KEYS) -> list:
     """Retrieve the values of a python dictionary.
 
     :param: py_dict: a python dictionary
@@ -82,18 +84,18 @@ def get_primary_id_from_equivalent_ids(equivalent_ids: dict, _type: str):
     :param: _type: the type of the bio-entity
     """
     if not equivalent_ids:
-        return ''
+        return ""
     if _type in id_ranks:
         id_rank = id_ranks.get(_type)
         # loop through id_rank, return the first found id
         for _item in id_rank:
             if equivalent_ids.get(_item):
-                return (_item + ':' + str(equivalent_ids[_item][0]))
+                return _item + ":" + str(equivalent_ids[_item][0])
     # if no id from id_rank found, return a random one from equivalent ids
     for k, v in equivalent_ids.items():
         if v:
-            return (k + ':' + str(v[0]))
-    return ''
+            return k + ":" + str(v[0])
+    return ""
 
 
 def get_name_from_equivalent_ids(equivalent_ids, input_label=None):
@@ -106,16 +108,17 @@ def get_name_from_equivalent_ids(equivalent_ids, input_label=None):
         return input_label
     if not equivalent_ids:
         return "unknown"
-    if equivalent_ids.get('SYMBOL'):
-        return equivalent_ids.get('SYMBOL')[0]
-    if equivalent_ids.get('name'):
-        return equivalent_ids.get('name')[0]
+    if equivalent_ids.get("SYMBOL"):
+        return equivalent_ids.get("SYMBOL")[0]
+    if equivalent_ids.get("name"):
+        return equivalent_ids.get("name")[0]
     for v in equivalent_ids.values():
         if v:
             if isinstance(v, list):
                 return v[0]
             return v
     return "unknown"
+
 
 def remove_prefix(_input, prefix):
     """Remove all prefixes in the input.
@@ -124,21 +127,43 @@ def remove_prefix(_input, prefix):
     :param: prefix: the prefix
     """
     if not prefix.endswith(":"):
-        prefix += ':'
+        prefix += ":"
     if not _input:
         return _input
     if isinstance(_input, str):
         if _input.startswith(prefix):
-            return _input[len(prefix):]
+            return _input[len(prefix) :]
         return _input
     if isinstance(_input, dict):
         new_result = {}
         for k, v in _input.items():
             if k.startswith(prefix):
-                new_result[k[len(prefix):]] = remove_prefix(v, prefix)
+                new_result[k[len(prefix) :]] = remove_prefix(v, prefix)
             else:
                 new_result[k] = remove_prefix(v, prefix)
         return new_result
     if isinstance(_input, list):
         return [remove_prefix(item, prefix) for item in _input]
     return _input
+
+
+def getPrefixFromCurie(curie: str) -> str:
+    return curie.split(":")[0]
+
+
+def getValFromCurie(curie: str) -> str:
+    prefix = curie.split(":")[0]
+    if prefix in ALWAYS_PREFIXED:
+        return curie
+    return curie.split(":", 1)[-1]
+
+
+def getCurieFromVal(val: str, prefix: str) -> str:
+    if prefix in ALWAYS_PREFIXED:
+        return val
+    return prefix + ":" + val
+
+
+def dict_set2list(input_dict: dict) -> dict:
+    return {k: list(v) for k, v in input_dict.items()}
+
