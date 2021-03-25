3bbac3c3a7b02ef14ddcbcdc9a89810d3031fecb
jiwen xin <kevinxin@scripps.edu>
2021-03-25 11:22:45 -0700

    test: :white_check_mark: add test for TRAPI class

--- tests/trapi/test_trapi.py
+++ tests/trapi/test_trapi.py
@@ -7,3 +7,21 @@ class TestTRAPIClass(unittest.TestCase):
         tp = TRAPI()
         with self.assertRaises(Exception):
             tp.url = {"a": "b"}
+
+    def test_url_is_correctly_set(self):
+        tp = TRAPI()
+        self.assertEqual(tp.url, "https://dev.api.bte.ncats.io/query")
+        tp.url = "https://api.bte.ncats.io/query"
+        self.assertEqual(tp.url, "https://api.bte.ncats.io/query")
+
+    def test_if_query_graph_not_dict_raise_exception(self):
+        tp = TRAPI()
+        qg = 1
+        with self.assertRaises(Exception):
+            tp.query_graph = qg
+
+    def test_query_graph_is_correctly_set(self):
+        qg = {"a": "b"}
+        tp = TRAPI()
+        tp.query_graph = qg
+        self.assertEqual(qg, tp.query_graph)
